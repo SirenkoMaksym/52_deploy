95d3ecea3ef85368d58f8bfafedbf6efc9223ad2
MaksymSirenko <brute1988@gmail.com>
2024-05-02 20:10:53 +0200

    vierunddreizigste

---
+++ lesson022/script.js
@@ -0,0 +1,23 @@
+const formItems = document.querySelector('#formItems')
+const content = document.querySelector('#content')
+const productList = documet.querySelector('#productList')
+const loader = document.querySelector()
+
+
+
+formItems.addEventListener('submit', e=>{
+    e.preventDefault()
+    const amount = e.target.amount.value
+    while()
+})
+
+
+
+
+
+
+
+
+
+
+
